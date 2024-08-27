f2b8948569e27277f90eba10ee60dc31a13f8ce0
nguyentom180620 <nguyentom180620@gmail.com>
2024-08-26 21:05:49 -0400

    Worked on GUI! Moved far from just in console to practicing with event listeners for a dynamic website

--- script.js
+++ script.js
@@ -36,83 +36,133 @@ function playRound(humanChoice, computerChoice) {
         case "rock":
             switch (computerChoice) {
                 case "rock":
-                    console.log("Tie! You both chose rock");
-                    break;
+                    return "Tie! You both chose rock";
                 case "paper":
-                    console.log("You lose! Your opponent chose paper");
                     computerScore++;
-                    break;
+                    return "You lose! Your opponent chose paper";
                 case "scissors":
-                    console.log("You win! Your opponent chose scissors");
                     humanScore++;
-                    break;
+                    return "You win! Your opponent chose scissors";
             }
             break;
         case "paper":
             switch (computerChoice) {
                 case "rock":
-                    console.log("You win! Your opponent chose rock");
                     humanScore++;
-                    break;
+                    return "You win! Your opponent chose rock";
                 case "paper":
-                    console.log("Tie! You both chose paper");
-                    break;
+                    return "Tie! You both chose paper";
                 case "scissors":
-                    console.log("You lose! Your opponent chose scissors");
                     computerScore++;
-                    break;
+                    return "You lose! Your opponent chose scissors";
             }
             break;
         case "scissors":
             switch (computerChoice) {
                 case "rock":
-                    console.log("You lose! Your opponent chose rock");
                     computerScore++;
-                    break;
+                    return "You lose! Your opponent chose rock";
                 case "paper":
-                    console.log("You win! Your opponent chose paper");
                     humanScore++;
-                    break;
+                    return "You win! Your opponent chose paper";
                 case "scissors":
-                    console.log("Tie! You both chose scissors");
-                    break;
+                    return "Tie! You both chose scissors";
             }
             break;
         default:
-            console.log("Uh-oh, we shouldn't be here D:");
-            break;
+            return "Uh-oh, we shouldn't be here D:";
     }
 }
 
 function playGame() {
-    let numberOfRounds = 5;
-    for (let i = 0; i < numberOfRounds; i++) {
-        let humanSelection;
+    let humanSelection;
+    humanSelection = getHumanChoice();
+    while (humanSelection === "Invalid choice") {
+        console.log("Invalid choice, please try again.");
         humanSelection = getHumanChoice();
-        if (humanSelection === "Invalid choice") {
-            console.log("Invalid choice, please try again.");
-            i--;
-            continue;
-        }
-        const computerSelection = getComputerChoice();
+    }
+    const computerSelection = getComputerChoice();
 
-        playRound(humanSelection, computerSelection);
-        console.log(`Your Score: ${humanScore}\nComputer Score: ${computerScore}\n`);
-    }
-    console.log('\n');
-    if (humanScore === computerScore) {
-        // Since tie rounds do not reset (this game is not best of 5, but rather just 5 rounds), this is very much possible
-        console.log("Woah! You guys tied!");
-    }
-    else if (humanScore > computerScore) {
-        console.log("You win!");
-    }
-    else {
-        console.log("You lose!");
-    }
+    playRound(humanSelection, computerSelection);
+    // console.log(`Your Score: ${humanScore}\nComputer Score: ${computerScore}\n`);
+    // console.log('\n');
+    // if (humanScore === computerScore) {
+    //     // Since tie rounds do not reset (this game is not best of 5, but rather just 5 rounds), this is very much possible
+    //     console.log("Woah! You guys tied!");
+    // }
+    // else if (humanScore > computerScore) {
+    //     console.log("You win!");
+    // }
+    // else {
+    //     console.log("You lose!");
+    // }
 }
 
-const button = document.querySelector("#btn");
-button.addEventListener("click", () => {
-    playGame();
+// Div DOM for results and game state reporting to user
+const playerResult = document.querySelector("#player-results");
+const computerResult = document.querySelector("#computer-results");
+const activityBox = document.querySelector("#activity");
+activityBox.textContent = "Game Activity goes here!";
+
+// Buttons for each selection
+// const rock = document.querySelector("#rock");
+// rock.addEventListener("click", () => {
+//     const computerSelection = getComputerChoice();
+//     playRound("rock", computerSelection);
+//     console.log(`Your Score: ${humanScore}\nComputer Score: ${computerScore}\n`);
+//     result.textContent = "Your Score: " +  humanScore;
+// });
+
+// const paper = document.querySelector("#paper");
+// paper.addEventListener("click", () => {
+//     const computerSelection = getComputerChoice();
+//     playRound("paper", computerSelection);
+//     console.log(`Your Score: ${humanScore}\nComputer Score: ${computerScore}\n`);
+//     result.textContent = "Your Score: " +  humanScore;
+// });
+
+// const scissors = document.querySelector("#scissors");
+// scissors.addEventListener("click", () => {
+//     const computerSelection = getComputerChoice();
+//     playRound("scissors", computerSelection);
+//     console.log(`Your Score: ${humanScore}\nComputer Score: ${computerScore}\n`);
+//     result.textContent = "Your Score: " +  humanScore;
+// });
+
+// Start Button
+
+const startButton = document.querySelector("#start-game");
+started = false;
+startButton.addEventListener("click", () => {
+    started = true;
+    humanScore = 0;
+    computerScore = 0;
+    playerResult.textContent = "Your Score: " +  humanScore;
+    computerResult.textContent = "Computer Score: " + computerScore;
+    activityBox.textContent = "Game Activity goes here!";
+});
+
+// Here is the buttons again, this time in a for loop for ease
+
+const buttons = document.querySelector(".buttons");
+buttons.childNodes.forEach(element => {
+    element.addEventListener("click", () => {
+        if (started) {
+            if (!(humanScore === 5 || computerScore === 5)) {
+                const humanChoice = element.textContent.toLowerCase();
+                const computerSelection = getComputerChoice();
+                activityBox.textContent = playRound(humanChoice, computerSelection);
+                playerResult.textContent = "Your Score: " +  humanScore;
+                computerResult.textContent = "Computer Score: " + computerScore;
+                if (humanScore === 5 || computerScore === 5) {
+                    if (humanScore > computerScore) {
+                        activityBox.textContent = "You win!";
+                    }
+                    else {
+                        activityBox.textContent = "You lose!";
+                    }
+                }
+            }
+        }
+    })
 });
